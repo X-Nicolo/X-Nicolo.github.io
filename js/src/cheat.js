986ed1dbee1f9c22ec29e58fb2b57f6eb77863b1
Nicolo <xiaosa.nicolo@qq.com>
2017-08-08 11:17:40 +0800

    Site updated: 2017-08-08 11:17:39

--- js/src/cheat.js
+++ js/src/cheat.js
@@ -1,15 +1,15 @@
-<!--������ƭ-->
+<!--崩溃欺骗-->
 var OriginTitile = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '�q(��A��`)�r ҳ�������� ~ | Nicolo��';
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~ | Nicolo！';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(?>��<*?) ���ֺ���~' + OriginTitile;
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
          titleTime = setTimeout(function () {
              document.title = OriginTitile;
          }, 2000);
